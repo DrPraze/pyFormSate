43782e7dc0c6946016830fee641cf324665da3f0
Praise James <66879837+DrPraze@users.noreply.github.com>
2022-08-23 20:45:03 +0100

    Update test.py

--- test.py
+++ test.py
@@ -1,5 +1,5 @@
 import requests
 
-BASE = "http://127.0.0.1:5000/getfields"
+BASE = "http://127.0.0.1:5000/getfields/"
 response = requests.get(BASE+"https://mirrorstoreng.com/register")
 print(response.json())
